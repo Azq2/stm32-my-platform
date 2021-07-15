661a742e5f4d2310d403c17fadd774d0e5c2ae92
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2021-07-15 23:46:17 +0300

    EXTI

---
+++ src/Exti.cpp
@@ -0,0 +1,112 @@
+#include "Exti.h"
+
+#include <libopencm3/stm32/gpio.h>
+
+#include <cstdio>
+
+Exti::Channel Exti::m_channels[EXTI_COUNT];
+
+int Exti::set(uint32_t bank, uint32_t pin, Trigger trigger, Callback *callback) {
+	uint8_t id = Gpio::pin2id(pin);
+	uint32_t exti = id2exti(id);
+	
+	taskENTER_CRITICAL();
+	if (m_channels[id].bank) {
+		taskEXIT_CRITICAL();
+		return ERR_EXISTS;
+	}
+	
+	m_channels[id].bank = bank;
+	m_channels[id].callback = callback;
+	
+	// Enable EXTI and connect to specified GPIO bank
+	exti_select_source(exti, bank);
+	exti_set_trigger(exti, (exti_trigger_type) trigger);
+	exti_enable_request(exti);
+	
+	// Enable IRQ for EXTI
+	int irq_n = getIrq(id);
+	nvic_enable_irq(irq_n);
+	nvic_set_priority(irq_n, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
+	
+	taskEXIT_CRITICAL();
+	
+	return ERR_SUCCESS;
+}
+
+int Exti::remove(uint32_t bank, uint32_t pin) {
+	int id = Gpio::pin2id(pin);
+	uint32_t exti = id2exti(id);
+	
+	taskENTER_CRITICAL();
+	if (!m_channels[id].bank || m_channels[id].bank != bank) {
+		taskEXIT_CRITICAL();
+		return ERR_SUCCESS;
+	}
+	
+	m_channels[id].bank = 0;
+	m_channels[id].callback = nullptr;
+	
+	// Disconnect GPIO bank from EXTI
+	exti_select_source(exti, 0);
+	
+	// Disable exti
+	exti_disable_request(exti);
+	
+	// Disable IRQ, if it not used by another EXTI
+	int irq_n = getIrq(id);
+	if (!isSharedIrqUsed(irq_n))
+		nvic_disable_irq(irq_n);
+	
+	taskEXIT_CRITICAL();
+	
+	return ERR_SUCCESS;
+}
+
+void Exti::handleIrq(uint8_t id) {
+	uint32_t exti = id2exti(id);
+	Channel &channel = m_channels[id];
+	
+	bool state = false;
+	if ((EXTI_RTSR & exti) && (EXTI_FTSR & exti)) {
+		uint16_t pin = Gpio::id2pin(id);
+		state = gpio_get(channel.bank, pin) != 0;
+	} else if ((EXTI_RTSR & exti)) {
+		state = true;
+	}
+	
+	exti_reset_request(exti);
+	channel.callback(state);
+}
+
+#ifdef STM32F4
+extern "C" void exti0_isr(void) {
+	Exti::handleIrq(0);
+}
+
+extern "C" void exti1_isr(void) {
+	Exti::handleIrq(1);
+}
+
+extern "C" void exti2_isr(void) {
+	Exti::handleIrq(2);
+}
+
+extern "C" void exti3_isr(void) {
+	Exti::handleIrq(3);
+}
+
+extern "C" void exti4_isr(void) {
+	Exti::handleIrq(4);
+}
+
+extern "C" void exti9_5_isr(void) {
+	Exti::handleIrqRange(5, 9);
+}
+
+extern "C" void exti15_19_isr(void) {
+	Exti::handleIrqRange(10, 15);
+}
+#else
+	#error Unsupported hardware
+#endif
---
+++ src/Exti.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "Gpio.h"
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/cm3/nvic.h>
+
+class Exti {
+	public:
+		enum Trigger: int {
+			RISING		= EXTI_TRIGGER_RISING,
+			FALLING		= EXTI_TRIGGER_FALLING,
+			BOTH		= EXTI_TRIGGER_BOTH
+		};
+		
+		typedef void (Callback)(bool state);
+	protected:
+		
+		static constexpr int EXTI_COUNT = 16;
+		
+		static constexpr uint32_t id2exti(int hub_id) {
+			return 1 << hub_id;
+		}
+		
+		struct Channel {
+			Callback *callback = nullptr;
+			uint32_t bank = 0;
+		};
+		
+		static Channel m_channels[EXTI_COUNT];
+		
+		static constexpr int getIrq(uint8_t id) {
+			#ifdef STM32F4
+			switch (id) {
+				case 0:		return NVIC_EXTI0_IRQ;
+				case 1:		return NVIC_EXTI1_IRQ;
+				case 2:		return NVIC_EXTI2_IRQ;
+				case 3:		return NVIC_EXTI3_IRQ;
+				case 4:		return NVIC_EXTI3_IRQ;
+				
+				case 5: case 6: case 7: case 8: case 9:
+					return NVIC_EXTI9_5_IRQ;
+				case 10: case 11: case 12: case 13: case 14: case 15:
+					return NVIC_EXTI15_10_IRQ;
+			}
+			#else
+				#error Unsupported hardware
+			#endif
+			return -1;
+		}
+		
+		static constexpr bool isSharedIrqUsed(int irq_n) {
+			#ifdef STM32F4
+			switch (irq_n) {
+				case NVIC_EXTI9_5_IRQ:
+					for (int i = 5; i <= 9; i++) {
+						if (m_channels[i].bank)
+							return true;
+					}
+				break;
+				
+				case NVIC_EXTI15_10_IRQ:
+					for (int i = 10; i <= 15; i++) {
+						if (m_channels[i].bank)
+							return true;
+					}
+				break;
+			}
+			#else
+				#error Unsupported hardware
+			#endif
+			return false;
+		}
+	public:
+		enum Errors {
+			ERR_SUCCESS		= 0,
+			ERR_EXISTS		= -1,
+		};
+		
+		static void handleIrq(uint8_t hub_id);
+		static inline void handleIrqRange(uint8_t from, uint8_t to) {
+			for (uint8_t i = from; i <= to; i++) {
+				if (exti_get_flag_status(id2exti(i)))
+					Exti::handleIrq(i);
+			}
+		};
+		static int set(uint32_t bank, uint32_t pin, Trigger trigger, Callback *callback);
+		static int remove(uint32_t bank, uint32_t pin);
+};
